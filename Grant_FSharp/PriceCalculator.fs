60f4365458081ba739c7f561cb2f968e630da405
Grant Crofton <grantac@hotmail.com>
2014-07-09 17:42:34 +0100

    Finished basic part of kata

--- Grant_FSharp/PriceCalculator.fs
+++ Grant_FSharp/PriceCalculator.fs
@@ -1,18 +1,39 @@
 ﻿module PriceCalculator
 
+[<Measure>]
+type percent
+
 type Book = 
     | Goblet
     | Wizard
+    | Demons
+    | Snakes
+    | Philosoper
 
-type Calculator(bookPrice, discounts:Map<int,float>) =
+type Calculator(bookPrice, discounts:Map<int,float<percent>>) =
     member this.GetPrice books =
 
-        let discountPercent = 
-            match books with
-            | x::y::z::[] when x=y&&y=z -> discounts.[3]
-            | x::y::[] when x=y -> discounts.[2]
-            | _ -> 0.0
+        let rec calculatePrice remainingBooks =
+            
+            match remainingBooks with
+            | [] -> 0.0
+            | _ ->
+                // group the books by title, including a count of how many
+                let bookGroups = remainingBooks |> Seq.countBy id |> List.ofSeq
+                
+                // build a list of the books which have not had a discount applied
+                let remaining = 
+                    bookGroups
+                    |> List.filter (fun (book,count) -> count > 1)
+                    |> List.map fst
 
-        let price = bookPrice * float (List.length books)
+                // work out the price (including discount) of the current set of books
+                let numDifferentBooks = Seq.length bookGroups
+                let discountPercent = float discounts.[numDifferentBooks]
+                let undiscountedPrice = bookPrice * (float numDifferentBooks)
+                let price = undiscountedPrice - (undiscountedPrice / 100.0 * discountPercent)
 
-        price - (price / 100.0 * discountPercent)
+                // return the current price plus the price of the remaining books, calculated recursively
+                price + (calculatePrice remaining)
+
+        calculatePrice books
--- Grant_FSharp/PriceCalculator_Tests.fs
+++ Grant_FSharp/PriceCalculator_Tests.fs
@@ -7,26 +7,32 @@ open Xunit
 let bookPrice = 5.0
 let discounts =
     Map.empty
-        .Add(2, 5.0)
-        .Add(3, 10.0)
+        .Add(1, 0.0<percent>)
+        .Add(2, 5.0<percent>)
+        .Add(3, 10.0<percent>)
+        .Add(4, 20.0<percent>)
+        .Add(5, 25.0<percent>)
 let calc = Calculator(bookPrice, discounts)
-
-//discounts
-//2 - 5%
-//3 - 10%
 
 [<Fact>]
 let ``single books gives single book price`` () =
     calc.GetPrice [Goblet] |> should equal bookPrice
 
 [<Fact>]
-let ``two different books gives undiscounted price`` () =
-    calc.GetPrice [Goblet;Wizard] |> should equal (bookPrice*2.0)
+let ``two same books gives undiscounted price`` () =
+    calc.GetPrice [Goblet;Goblet] |> should equal (bookPrice*2.0)
 
 [<Fact>]
-let ``two same books gives discounted price`` () =
-    calc.GetPrice [Goblet;Goblet] |> should equal 9.5
+let ``different books gives discounted price`` () =
+    calc.GetPrice [Goblet;Wizard] |> should equal 9.5 // 5*2-5%
+    calc.GetPrice [Goblet;Wizard;Demons] |> should equal 13.5 // 5*3-10%
+    calc.GetPrice [Goblet;Wizard;Demons;Snakes] |> should equal 16.0 // 5*4-20%
+    calc.GetPrice [Goblet;Wizard;Demons;Snakes;Philosoper] |> should equal 18.75 // 5*5-25%
 
 [<Fact>]
-let ``three same books gives discounted price`` () =
-    calc.GetPrice [Goblet;Goblet;Goblet] |> should equal 13.5
+let ``mix of different and same books applies discount to different ones`` () =
+    calc.GetPrice [Goblet;Demons;Goblet] |> should equal 14.5 // 5*2-5% + 5
+
+[<Fact>]
+let ``multiple sets of same books applies discounts`` () =
+    calc.GetPrice [Goblet;Demons;Goblet;Demons;Wizard;Philosoper] |> should equal 25.5 // 5*2-5% + 5*4-20%
