72ca9314b6f7baafdb0a9b790161fd8c13db4057
Grant Crofton <grantac@hotmail.com>
2014-07-09 09:00:22 +0100

    Started implementing kata

--- Grant_FSharp/PriceCalculator.fs
+++ Grant_FSharp/PriceCalculator.fs
@@ -1,4 +1,18 @@
 ﻿module PriceCalculator
 
-let calc x = 
-    0
+type Book = 
+    | Goblet
+    | Wizard
+
+type Calculator(bookPrice, discounts:Map<int,float>) =
+    member this.GetPrice books =
+
+        let discountPercent = 
+            match books with
+            | x::y::z::[] when x=y&&y=z -> discounts.[3]
+            | x::y::[] when x=y -> discounts.[2]
+            | _ -> 0.0
+
+        let price = bookPrice * float (List.length books)
+
+        price - (price / 100.0 * discountPercent)
--- Grant_FSharp/PriceCalculator_Tests.fs
+++ Grant_FSharp/PriceCalculator_Tests.fs
@@ -4,6 +4,29 @@ open PriceCalculator
 open FsUnit
 open Xunit
 
+let bookPrice = 5.0
+let discounts =
+    Map.empty
+        .Add(2, 5.0)
+        .Add(3, 10.0)
+let calc = Calculator(bookPrice, discounts)
+
+//discounts
+//2 - 5%
+//3 - 10%
+
 [<Fact>]
-let ``whatever`` () =
-    calc 123 |> should equal 0
+let ``single books gives single book price`` () =
+    calc.GetPrice [Goblet] |> should equal bookPrice
+
+[<Fact>]
+let ``two different books gives undiscounted price`` () =
+    calc.GetPrice [Goblet;Wizard] |> should equal (bookPrice*2.0)
+
+[<Fact>]
+let ``two same books gives discounted price`` () =
+    calc.GetPrice [Goblet;Goblet] |> should equal 9.5
+
+[<Fact>]
+let ``three same books gives discounted price`` () =
+    calc.GetPrice [Goblet;Goblet;Goblet] |> should equal 13.5
